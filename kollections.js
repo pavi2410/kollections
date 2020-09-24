058776bbe0f8c78d4bc3695383d1c9a63ab18ab5
Pavitra Golchha <pavitragolchha@gmail.com>
2020-09-24 10:31:37 +0530

    Update kollections.js

--- kollections.js
+++ kollections.js
@@ -1,24 +1,99 @@
-function listOf(...init) {
-    let array = init || [];
-    let proxy = new Proxy(array, {
-        get(target, prop) {
-            prop = prop.split(',').map(n => parseInt(n, 10));
+class List extends Array {
+  constructor(items) {
+    super(...items)
+  }
 
-            switch (prop.length) {
-                case 1:
-                    if (!isNaN(prop)) {
-                        if (prop < 0) {
-                            prop += target.length;
-                        }
-                    }
-                    return target[prop];
-                case 2:
-                case 3:
-                    return target.slice(...prop);
-                default:
-                    throw new Error('invalid array access');
-            }
+  [Symbol.iterator]() {
+    let i = 0;
+
+    return {
+      next: () => {
+        if (i < this.length) {
+          return {value: this[i++], done: false}
+        } else {
+          return {done: true}
         }
-    });
-    return proxy;
+      }
+    }
+  }
+
+  get size() {
+    return this.length
+  }
+
+  get first() {
+    return this[0]
+  }
+
+  get last() {
+    return this[this.size - 1]
+  }
+
+  get(i) {
+    return this[i]
+  }
+
+  // mutable!!
+  set(i, k) {
+    this[i] = k
+  }
+
+  // mutable!!
+  add(k) {
+    this.push(k)
+  }
+
+  // mutable!!
+  remove(i) {
+    // this = this.slice(0, i) + this.slice(i+1)
+  }
+
+  contains(k) {
+    return k in this
+  }
+
+  isEmpty() {
+    return this.size === 0
+  }
+
+  joinToString(sep = ',') {
+    return this.join(sep)
+  }
+
+  reverse() {
+    return new List(this.toArray().reverse())
+  }
+
+  filter(cb) {
+    return new List(this.toArray().filter(cb))
+  }
+
+  forEach(cb) {
+    this.toArray().forEach(cb)
+  }
+  
+  onEach(cb) {
+    this.toArray().forEach(cb)
+    return this
+  }
+
+  map(cb) {
+    return new List(this.toArray().map(cb))
+  }
+  
+  toString() {
+    return `List [${this.toArray()}]`
+  }
+
+  toArray() {
+    return [...this]
+  }
+
+  get [Symbol.toStringTag]() {
+    return 'List'
+  }
+}
+
+export function listOf(...items) {
+  return new List(items)
 }
